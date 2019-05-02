4deeddeaa774411b4b20258b76881417deeba906
Brian DiRito <bdirito91@gmail.com>
2019-05-02 11:34:24 -0400

    updated JS notes

--- assessments/javascript/spyGame.js
+++ assessments/javascript/spyGame.js
@@ -1,11 +1,11 @@
 const firstName = prompt("What is your first name?")
 const lastName = prompt("What is your last name?")
-const age = prompt("How old are you?")
+const age = Number(prompt("How old are you?"))
 const height = prompt("How tall are you in centimeters? (Height in inches * 2.54)")
 const pet = prompt("What is the name of your pet?(Enter \"none\" if you don't have a pet)")
 
 if (firstName[0].toLowerCase() === lastName[0].toLowerCase()){
-    if (Number(age) > 20 && Number(age) < 30){
+    if (age > 20 && age < 30){
         if (height >= 170){
             if (pet[pet.length - 1] === "y"){
                 alert("Find the secret message")
--- front_end_notes/javascript/basics.js
+++ front_end_notes/javascript/basics.js
@@ -2,4 +2,4 @@
 
 // \t will give you a tab
 
-// 
+// when dealing with global variables, they don't need to be passed as an argument in a function
