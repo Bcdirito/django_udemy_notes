d0a62de3a3ea9792fc6a8de2bb5090214dfd3b1e
Brian DiRito <bdirito91@gmail.com>
2019-05-01 22:24:49 -0400

    revised first exercise

--- assessments/javascript/converter.js
+++ assessments/javascript/converter.js
@@ -1,4 +1,4 @@
-let lbs = prompt("Please Enter a Weight")
-const kgs = Number(lbs) * 0.454
+let lbs = Number(prompt("Please Enter a Weight"))
+const kgs = lbs * 0.454
 alert(`${lbs}lbs is actually ${kgs}kgs`)
 console.log("YOU DID IT!")
