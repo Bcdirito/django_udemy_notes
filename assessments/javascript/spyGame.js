a8861b525e1fa3ca319aacc44ce65b71f4c1f3c7
Brian DiRito <bdirito91@gmail.com>
2019-05-02 11:25:25 -0400

    final spygame revision

--- assessments/javascript/spyGame.js
+++ assessments/javascript/spyGame.js
@@ -1,13 +1,16 @@
 const firstName = prompt("What is your first name?")
 const lastName = prompt("What is your last name?")
+const age = prompt("How old are you?")
 const height = prompt("How tall are you in centimeters? (Height in inches * 2.54)")
 const pet = prompt("What is the name of your pet?(Enter \"none\" if you don't have a pet)")
 
 if (firstName[0].toLowerCase() === lastName[0].toLowerCase()){
-    if (height >= 170){
-        if (pet[pet.length - 1] === "y"){
-            alert("Find the secret message")
-            console.log("I see u spyin'")
+    if (Number(age) > 20 && Number(age) < 30){
+        if (height >= 170){
+            if (pet[pet.length - 1] === "y"){
+                alert("Find the secret message")
+                console.log("I see u spyin'")
+            }
         }
     }
 }
