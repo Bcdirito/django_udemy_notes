d475ba397bb0c028a579f3dd47b153ae804f488b
Brian DiRito <bdirito91@gmail.com>
2019-05-02 15:00:05 -0400

    updated dom_notes

---
+++ front_end_notes/javascript/dom/dom_notes.js
@@ -0,0 +1,9 @@
+// DOM Notes
+
+// utilize console.dir() to get a better view of DOM models
+
+// document.links grabs all links on page
+
+// querySelectors allows you to us css selectors
+
+// .style allows you to interact with css elements
