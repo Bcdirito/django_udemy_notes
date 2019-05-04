45f99648a1ebd3e335f40a424dcf5198120b9a7c
Brian DiRito <bdirito91@gmail.com>
2019-05-04 15:25:14 -0400

    added tuples and sets notes

---
+++ back_end_notes/python/tuples_and_sets.py
@@ -0,0 +1,24 @@
+# Tuples, Sets, and Booleans
+
+# Tuples are immutable sequences
+# Sets are unordered collections of unique elements
+
+# Booleans are capitalized in Python
+
+# Tuple Examples
+t = (1, 2, 3)
+print(t[0])
+
+# Can hold multiple data types
+
+# Set Examples
+x = set()
+x.add(1)
+x.add(2)
+x.add(4)
+x.add(4)
+x.add(4)
+
+print(x)
+
+# x will still only equal {1, 2, 4}
