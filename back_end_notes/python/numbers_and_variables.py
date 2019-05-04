407a587e48aaa73dbf8de6e7aab43a7330848dd2
Brian DiRito <bdirito91@gmail.com>
2019-05-04 14:43:24 -0400

    added numbers and variable python notes

---
+++ back_end_notes/python/numbers_and_variables.py
@@ -0,0 +1,13 @@
+# Numbers and Variables Notes
+
+# To access Python terminal, type python into terminal
+# To quit, input quit()
+
+# Square Root Example
+print(4**0.5)
+
+# To run file type python and then relative file path
+
+# Variable names cannot start with a number
+
+#  If one number in an operation is a float, the end result is a float
