2f09dd6e3ea5f55605b8275d7191235ad707784a
Brian DiRito <bdirito91@gmail.com>
2019-05-06 12:37:13 -0400

    did python functions assessment

---
+++ assessments/python/level_one/Part9_Functions_Exercises.py
@@ -0,0 +1,158 @@
+#####################################
+#### PART 9: FUNCTION EXERCISES #####
+#####################################
+
+
+# Complete the tasks below by writing functions! Keep in mind, these can be
+# really tough, its all about breaking the problem down into smaller, logical
+# steps. If you get stuck, don't feel bad about having to peek to the solutions!
+
+#####################
+## -- PROBLEM 1 -- ##
+#####################
+
+# Given a list of integers, return True if the sequence of numbers 1, 2, 3
+# appears in the list somewhere.
+
+# For example:
+
+# arrayCheck([1, 1, 2, 3, 1]) → True
+# arrayCheck([1, 1, 2, 4, 1]) → False
+# arrayCheck([1, 1, 2, 1, 2, 3]) → True
+
+def arrayCheck(nums):
+  # CODE GOES HERE
+  counter = -1
+
+  for n in nums:
+    counter += 1
+    if (n == 1) and (nums[counter + 1] == 2) and (nums[counter + 2] == 3):
+      return True
+    elif counter >= len(nums) - 3:
+      return False
+
+#####################
+## -- PROBLEM 2 -- ##
+#####################
+
+# Given a string, return a new string made of every other character starting
+# with the first, so "Hello" yields "Hlo".
+
+# For example:
+
+# stringBits('Hello') → 'Hlo'
+# stringBits('Hi') → 'H'
+# stringBits('Heeololeo') → 'Hello'
+
+def stringBits(string):
+  # CODE GOES HERE
+  return (string[0::2])
+
+
+#####################
+## -- PROBLEM 3 -- ##
+#####################
+
+# Given two strings, return True if either of the strings appears at the very end
+# of the other string, ignoring upper/lower case differences (in other words, the
+# computation should not be "case sensitive").
+#
+# Note: s.lower() returns the lowercase version of a string.
+#
+# Examples:
+#
+# end_other('Hiabc', 'abc') → True
+# end_other('AbC', 'HiaBc') → True
+# end_other('abc', 'abXabc') → True
+
+
+def end_other(a, b):
+  # CODE GOES HERE
+  if (len(a) > len(b)):
+    sub_a_origin = len(a) - len(b)
+    sub_a = a[sub_a_origin:]
+    return sub_a.lower() == b.lower()
+  else:
+    sub_b_origin = len(b) - len(a)
+    sub_b = b[sub_b_origin:]
+    return sub_b.lower() == a.lower()
+
+#####################
+## -- PROBLEM 4 -- ##
+#####################
+
+# Given a string, return a string where for every char in the original,
+# there are two chars.
+
+# doubleChar('The') → 'TThhee'
+# doubleChar('AAbb') → 'AAAAbbbb'
+# doubleChar('Hi-There') → 'HHii--TThheerree'
+
+def doubleChar(string):
+  # CODE GOES HERE
+  double_str = ""
+
+  for s in string:
+    double_str += s
+    double_str += s
+
+  return double_str
+
+#####################
+## -- PROBLEM 5 -- ##
+#####################
+
+# Read this problem statement carefully!
+
+# Given 3 int values, a b c, return their sum. However, if any of the values is a
+# teen -- in the range 13-19 inclusive -- then that value counts as 0, except 15
+# and 16 do not count as a teens. Write a separate helper "def fix_teen(n):"that
+# takes in an int value and returns that value fixed for the teen rule.
+#
+# In this way, you avoid repeating the teen code 3 times (i.e. "decomposition").
+# Define the helper below and at the same indent level as the main no_teen_sum().
+# Again, you will have two functions for this problem!
+#
+# Examples:
+#
+# no_teen_sum(1, 2, 3) → 6
+# no_teen_sum(2, 13, 1) → 3
+# no_teen_sum(2, 1, 14) → 3
+
+def no_teen_sum(*args):
+  # CODE GOES HERE
+  final_num = 0
+  for a in args:
+    final_num += fix_teen(a)
+  
+  return final_num
+
+def fix_teen(num):
+  teens = list(range(10, 20))
+  if num in teens and (num != 15 or num != 16):
+    return 0
+  else:
+    return num
+
+
+#####################
+## -- PROBLEM 6 -- ##
+#####################
+
+# Return the number of even integers in the given array.
+#
+# Examples:
+#
+# count_evens([2, 1, 2, 3, 4]) → 3
+# count_evens([2, 2, 0]) → 3
+# count_evens([1, 3, 5]) → 0
+
+def count_evens(nums):
+  # CODE GOES HERE
+  evens = 0
+  for n in nums:
+    if n%2==0:
+      evens += 1
+
+  return evens
+
