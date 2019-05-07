628efbc10cb0d4fa020361958be7aa59707cefa2
Brian DiRito <bdirito91@gmail.com>
2019-05-07 13:49:43 -0400

    added modules and packages notes:

---
+++ back_end_notes/python/level_two/modules_and_packages/modules_and_packages.py
@@ -0,0 +1,4 @@
+# Modules and Packages Notes
+
+def func_in_module():
+    print("I AM INSIDE THE MODULE NOTES FILE")
---
+++ back_end_notes/python/level_two/modules_and_packages/my_program.py
@@ -0,0 +1,9 @@
+# Shortens the name of an import
+# Same like imports from React
+# import modules_and_packages as mm
+
+# Or you can import specific functions
+from modules_and_packages import func_in_module
+
+# Call function from another file
+func_in_module()
