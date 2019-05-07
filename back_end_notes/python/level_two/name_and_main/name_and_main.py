8eea854c144b92b094b5e6c5221d1e8b7d4194d5
Brian DiRito <bdirito91@gmail.com>
2019-05-07 14:14:27 -0400

    added name and main notes

---
+++ back_end_notes/python/level_two/name_and_main/name_and_main.py
@@ -0,0 +1,13 @@
+# Name and main Notes
+
+# Example
+
+def func():
+    print("func() in name_and_main.py")
+
+print("TOP LEVEL NAME_AND_MAIN.py")
+
+if __name__ == "__main__":
+    print("name_and_main.py being run directly")
+else:
+    print("name_and_main.py being imported")
---
+++ back_end_notes/python/level_two/name_and_main/name_and_main_two.py
@@ -0,0 +1,8 @@
+import name_and_main
+
+print("TOP LEVEL NAME_AND_MAIN_TWO.py")
+
+if __name__ == "__main__":
+    print("name_and_main_two.py being run directly")
+else:
+    print("name_and_main_two.py is being imported")
