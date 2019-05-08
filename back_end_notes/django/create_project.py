29d044ccd4480c4e6df5b736090cc2d2024a4f92
Brian DiRito <bdirito91@gmail.com>
2019-05-08 12:56:56 -0400

    added create_project django notes

---
+++ back_end_notes/django/create_project.py
@@ -0,0 +1,25 @@
+# Create Project Notes
+
+# To create project:
+# Activate Django environment -> conda activate EnvironmentName
+# Then input django-admin startproject first_project
+
+# _init__.py file:
+# Directory can be treated as package
+
+# settings.py:
+# Project settings
+
+# urls.py:
+# Stores all of the urls
+# Makes use of a lot of regex
+
+
+# wsgi.py:
+# Helps with production deploy
+
+# manage.py:
+# Associated with many of the command
+
+# To start server -> input python relative_file_path runserver
+
