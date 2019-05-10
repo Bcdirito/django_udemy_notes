03204bd1f872396b58d49cf79b95520518c5ede2
Brian DiRito <bdirito91@gmail.com>
2019-05-10 12:31:41 -0400

    completed url mappings_notes

---
+++ back_end_notes/django/url_mappings.py
@@ -0,0 +1,8 @@
+# Url Mappings Notes
+
+# Importing Include
+from django.urls import include, path
+
+# Include path(r'^first_app/', include("first_app.urls")), in urlpatterns
+
+# Create urls.py file in application
