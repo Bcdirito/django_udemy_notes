ffcf052ef696e9ddc7951ba4b3334cee7ed3cd8d
Brian DiRito <bdirito91@gmail.com>
2019-05-10 12:32:42 -0400

    revised url mapping notes

--- back_end_notes/django/url_mappings.py
+++ back_end_notes/django/url_mappings.py
@@ -3,6 +3,7 @@
 # Importing Include
 from django.urls import include, path
 
-# Include path(r'^first_app/', include("first_app.urls")), in urlpatterns
+# Include path('app_directory_name', include("app_directory_name.urls")), in urlpatterns
 
 # Create urls.py file in application
+# It is similar to urls.py in project directory
