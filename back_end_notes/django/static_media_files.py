5d8e4b7a00d7aa57e8cd2680061d608bbd707d87
Brian DiRito <bdirito91@gmail.com>
2019-05-10 17:13:04 -0400

    added static media notes

---
+++ back_end_notes/django/static_media_files.py
@@ -0,0 +1,11 @@
+# Static File Notes
+
+# Very similiar creation to templates file
+# {% %} for more complex injections and logic
+
+# Any file that isn't going to change
+# Can include CSS files
+
+# add {% load staticfiles %} at top of template to be able to call static files
+
+# to reference file -> {% static "relative_file_path"%}
