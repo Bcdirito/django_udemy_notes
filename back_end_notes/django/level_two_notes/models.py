e7c6869f2ce01a131b11a315e2f55bc174b2b1e8
Brian DiRito <bdirito91@gmail.com>
2019-05-13 13:08:03 -0400

    added models notes part 1

---
+++ back_end_notes/django/level_two_notes/models.py
@@ -0,0 +1,35 @@
+# Django Models Notes
+
+# Django uses SQLite by default
+
+# Inherits from django.db.models.Model
+
+# Fields have constraints
+
+# Examples:
+
+class Topic(models.Model):
+    top_name = models.CharField(max_length = 264, unique=True)
+
+class Webpage(models.Model):
+    category = models.ForeignKey(Topic)
+    name = models.CharField(max_length = 264)
+    url = models.URLField()
+
+# To migrate -> input python manage.py migrate
+
+# To make changes -> python manage.py makemigrations app_name
+
+# Must migrate one more time after running makemigrations
+
+# Must register models with admin.py file
+
+# Example:
+from django.contrib import admin
+from app.models import Model1, Model2
+    admin.site.register(Model1)
+    admin.site.register(Model2)
+
+# Creating a superuser -> python manage.py createsuperuser
+# Then input name, email, and password
+
