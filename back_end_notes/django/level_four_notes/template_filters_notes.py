d21db7502c52ba84b8f2b83d6cf4ce1daee47f75
Brian DiRito <bdirito91@gmail.com>
2019-05-21 14:59:17 -0400

    completed notes for template filters

---
+++ back_end_notes/django/level_four_notes/template_filters_notes.py
@@ -0,0 +1,12 @@
+# Template Filters Notes
+
+# Add load call of {% load template_fitler_file_name.py  at top of template.html file %}
+
+# General Examples:
+
+{{ value | filter:"parameter" }}
+
+{{ django| title }}
+
+
+
