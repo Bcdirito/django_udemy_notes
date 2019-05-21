2cff6f53f9393d7cbfdf45d6cb2419f15bf7698e
Brian DiRito <bdirito91@gmail.com>
2019-05-21 15:11:28 -0400

    completed codealong template filters

--- back_end_notes/django/level_four_notes/template_inheritance.py
+++ back_end_notes/django/level_four_notes/template_inheritance.py
@@ -3,3 +3,6 @@
 # Sometimes also known as template extending
 
 # Allows for html to exist across all pages without rewriting it
+
+# Custom Filters
+# Create function in application at same level of models/views
