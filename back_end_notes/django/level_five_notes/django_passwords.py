9cda9f3bf4cb8d5491c096d7ee22d7a1e76b422a
Brian DiRito <bdirito91@gmail.com>
2019-05-22 12:14:09 -0400

    completed django password setup

---
+++ back_end_notes/django/level_five_notes/django_passwords.py
@@ -0,0 +1,11 @@
+# Passwords Notes
+
+# NEVER STORE PASSWORDS AS PLAIN TEXT
+
+# Password Hashing built into Django
+
+# Pass in list of PASSWORD_HASHERS
+
+# Validator for Stronger Passwords
+
+# Collision Resistance to keep data unique and protected
